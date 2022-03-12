dc0d16a0b5887a0ad97e35e056de0d51c9b70f10
Maxine Michalski <maxine_red1@yahoo.com>
2022-03-13 00:10:25 +0100

    Fixed oregen recipes

--- oregen.rb
+++ oregen.rb
@@ -5,6 +5,9 @@ require 'json'
 Dir.chdir(File.dirname(__FILE__)) do
   data = JSON.parse(File.read('data/oregen.json'), symbolize_names: true)
   botania = { values: [] }
+  Dir['kubejs/data/occultism/recipes/miner/**/*.json'].each do |f|
+    File.unlink(f)
+  end
   data.each do |ore|
     if ore[:category].include?('orechid_ores')
       botania[:values] << { type: 'block', block: ore[:block], weight: ore[:weight] }
@@ -13,7 +16,11 @@ Dir.chdir(File.dirname(__FILE__)) do
     unless form.empty?
       form = form.first.sub(/miner-/, '')
       miner = { type: 'occultism:miner', ingredient: { tag: "occultism:miners/#{form}" }, result: { item: ore[:block] }, weight: ore[:weight] }
-      File.write("kubejs/data/occultism/recipes/miner/#{form}/#{ore[:block].sub(/.+:/, '')}.json", JSON.pretty_generate(miner))
+      ore_name = ore[:block].sub(/.+:/, '')
+      if File.exists?("kubejs/data/occultism/recipes/miner/#{form}/#{ore_name}.json")
+        ore_name = ore[:block].sub(/:/, '_')
+      end
+      File.write("kubejs/data/occultism/recipes/miner/#{form}/#{ore_name}.json", JSON.pretty_generate(miner))
     end
   end
   File.write('kubejs/data/botania/orechid_ore_weights/orechid.json', JSON.pretty_generate(botania))
